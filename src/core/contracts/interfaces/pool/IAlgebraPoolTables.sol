f3c79df31dc2f30e6bdb2d3f72ef4c63f9a38cf7
sebyastiens <sebyastiens@gmail.com>
2024-02-14 10:50:43 +0100

    Create IAlgebraPoolTables.sol

---
+++ src/core/contracts/interfaces/pool/IAlgebraPoolTables.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.5.0;
+
+/**
+ * @title Pool state that is not stored
+ * @notice Contains view functions to provide information about the pool that is computed rather than stored on the
+ * blockchain. The functions here may have variable gas costs.
+ * @dev Credit to Uniswap Labs under GPL-2.0-or-later license:
+ * https://github.com/Uniswap/v3-core/tree/main/contracts/interfaces
+ */
+
+interface IAlgebraPoolTables  {
+
+    struct RangeDatas {
+    int24 tick;
+    uint256 MaxInjectable;
+    uint256 MaxReceived;
+    uint160 Price;
+    uint128 InRangeLiquidity;
+    }
+
+    function setPool (address _algebraPoolAddress, uint256 LoopLength) external returns (
+      RangeDatas[] memory Max_Injectable_Token0,
+      RangeDatas[] memory Max_Injectable_Token1
+    );
+  function GetMaxSwapTables(uint256 LoopLength)
+    external
+    returns (
+      RangeDatas[] memory Max_Injectable_Token0,
+      RangeDatas[] memory Max_Injectable_Token1
+    );
+
+}
