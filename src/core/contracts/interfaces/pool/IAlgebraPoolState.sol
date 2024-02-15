6bb3973c0de40f462c62e960101d65102ed42317
sebyastiens <sebyastiens@gmail.com>
2024-02-15 03:08:35 +0100

    Update IAlgebraPoolState.sol
    REMOVED :
    
      /**
       * @notice Returns the information about a position by the position's key
       * @dev This is a public mapping of structures, so the `return` natspec tags are omitted.
       * @param key The position's key is a hash of a preimage composed by the owner, bottomTick and topTick
       * @return liquidityAmount The amount of liquidity in the position;
       * Returns lastLiquidityAddTimestamp Timestamp of last adding of liquidity;
       * Returns innerFeeGrowth0Token Fee growth of token0 inside the tick range as of the last mint/burn/poke;
       * Returns innerFeeGrowth1Token Fee growth of token1 inside the tick range as of the last mint/burn/poke;
       * Returns fees0 The computed amount of token0 owed to the position as of the last mint/burn/poke;
       * Returns fees1 The computed amount of token1 owed to the position as of the last mint/burn/poke
       */
      function positions(bytes32 key)
        external
        view
        returns (
          uint128 liquidityAmount,
          uint32 lastLiquidityAddTimestamp,
          uint256 innerFeeGrowth0Token,
          uint256 innerFeeGrowth1Token,
          uint128 fees0,
          uint128 fees1
        );

--- src/core/contracts/interfaces/pool/IAlgebraPoolState.sol
+++ src/core/contracts/interfaces/pool/IAlgebraPoolState.sol
@@ -86,28 +86,7 @@ interface IAlgebraPoolState {
   /** @notice Returns 256 packed tick initialized boolean values. See TickTable for more information */
   function tickTable(int16 wordPosition) external view returns (uint256);
 
-  /**
-   * @notice Returns the information about a position by the position's key
-   * @dev This is a public mapping of structures, so the `return` natspec tags are omitted.
-   * @param key The position's key is a hash of a preimage composed by the owner, bottomTick and topTick
-   * @return liquidityAmount The amount of liquidity in the position;
-   * Returns lastLiquidityAddTimestamp Timestamp of last adding of liquidity;
-   * Returns innerFeeGrowth0Token Fee growth of token0 inside the tick range as of the last mint/burn/poke;
-   * Returns innerFeeGrowth1Token Fee growth of token1 inside the tick range as of the last mint/burn/poke;
-   * Returns fees0 The computed amount of token0 owed to the position as of the last mint/burn/poke;
-   * Returns fees1 The computed amount of token1 owed to the position as of the last mint/burn/poke
-   */
-  function positions(bytes32 key)
-    external
-    view
-    returns (
-      uint128 liquidityAmount,
-      uint32 lastLiquidityAddTimestamp,
-      uint256 innerFeeGrowth0Token,
-      uint256 innerFeeGrowth1Token,
-      uint128 fees0,
-      uint128 fees1
-    );
+
 
   /**
    * @notice Returns data about a specific timepoint index
