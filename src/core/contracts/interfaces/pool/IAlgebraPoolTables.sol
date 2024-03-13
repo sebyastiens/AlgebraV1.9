25758061e1cd2e023be012d13611fffca1df3c5d
sebyastiens <sebyastiens@gmail.com>
2024-03-13 05:56:30 +0100

    Update IAlgebraPoolTables.sol
    added decimals return

--- src/core/contracts/interfaces/pool/IAlgebraPoolTables.sol
+++ src/core/contracts/interfaces/pool/IAlgebraPoolTables.sol
@@ -15,42 +15,50 @@ pragma abicoder v2;
 interface IAlgebraPoolTables  {
 
     struct RangeDatas {
-    int24 tick;
-    uint256 MaxInjectable;
-    uint256 MaxReceived;
-    uint160 Price;
-    uint128 InRangeLiquidity;
+        int24 tick;
+        uint256 MaxInjectable;
+        uint256 MaxReceived;
+        uint160 Price;
+        uint128 InRangeLiquidity;
+    }
+
+    struct Token {
+        address Address;
+        string name;
+        string symbol;
+        uint8 decimals;
     }
 
     struct Cached {
-    uint128 volumePerLiquidityInBlock;
-    int56 tickCumulative; // The global tickCumulative at the moment
-    uint160 secondsPerLiquidityCumulative; // The global secondPerLiquidity at the moment
-    bool computedLatestTimepoint; //  if we have already fetched _tickCumulative_ and _secondPerLiquidity_ from the DataOperator
-    int256 amountRequiredInitial; // The initial value of the exact input\output amount
-    int256 amountCalculated; // The additive amount of total output\input calculated trough the swap
-    uint16 fee; // The current dynamic fee when zeroToOne is true -> swapping token0 for token1
-    int24 startTick; // The tick at the start of a swap
-    uint16 timepointIndex; // The index of last written timepoint
-    uint32 blockTimeStamp ; // time when the timepointIndex was created
-
-    uint160 stepSqrtPrice; // The Q64.96 sqrt of the price at the start of the step
-    int24 nextTick; // The tick till the current step goes
-    bool initialized; // True if the _nextTick is initialized
-    uint160 nextTickPrice; // The Q64.96 sqrt of the price calculated from the _nextTick
-    uint256 input; // The additive amount of tokens that have been provided
-    uint256 output; // The additive amount of token that have been withdrawn
-    uint256 feeAmount; // The total amount of fee earned within a current step
-
-    uint160 currentPrice;
-    int24 currentTick;
-    uint128 currentLiquidity;
+        uint128 volumePerLiquidityInBlock;
+        int56 tickCumulative; // The global tickCumulative at the moment
+        uint160 secondsPerLiquidityCumulative; // The global secondPerLiquidity at the moment
+        bool computedLatestTimepoint; //  if we have already fetched _tickCumulative_ and _secondPerLiquidity_ from the DataOperator
+        int256 amountRequiredInitial; // The initial value of the exact input\output amount
+        int256 amountCalculated; // The additive amount of total output\input calculated trough the swap
+        uint16 fee; // The current dynamic fee when zeroToOne is true -> swapping token0 for token1
+        int24 startTick; // The tick at the start of a swap
+        uint16 timepointIndex; // The index of last written timepoint
+        uint32 blockTimeStamp ; // time when the timepointIndex was created
+    
+        uint160 stepSqrtPrice; // The Q64.96 sqrt of the price at the start of the step
+        int24 nextTick; // The tick till the current step goes
+        bool initialized; // True if the _nextTick is initialized
+        uint160 nextTickPrice; // The Q64.96 sqrt of the price calculated from the _nextTick
+        uint256 input; // The additive amount of tokens that have been provided
+        uint256 output; // The additive amount of token that have been withdrawn
+        uint256 feeAmount; // The total amount of fee earned within a current step
+    
+        uint160 currentPrice;
+        int24 currentTick;
+        uint128 currentLiquidity;
   }
 
 
     function setPool (address _algebraPoolAddress, uint256 LoopLength) external returns (
       RangeDatas[] memory Max_Injectable_Token0,
-      RangeDatas[] memory Max_Injectable_Token1
+      RangeDatas[] memory Max_Injectable_Token1,
+      Token[2] memory TokensData
        // DataStorage.Timepoint[15] memory  timepointsMemory
     );
 
